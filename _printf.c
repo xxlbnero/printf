966b757744d83b733a0792bc16c80d0019cebfd5
Nerus <n.nwoko2023@gmail.com>
2023-03-27 12:34:51 -0700

    produce output according to format spec

--- _printf.c
+++ _printf.c
@@ -11,39 +11,37 @@
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, j = 0, len = 0;
-    va_list args;
-    char buffer[1024];
+	int i = 0, j = 0, len = 0;
+    	va_list args;
+    	char buffer[1024];
 
-    if (format == NULL)
-        return (-1);
+    	if (format == NULL)
+        	return (-1);
 
-    va_start(args, format);
+    	va_start(args, format);
 
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            j = handle_format_specifier(format, i + 1, &args, buffer);
-            if (j == -1)
-                return (-1);
-            len += j;
-            i++;
-        }
-        else
-        {
-            buffer[len++] = format[i];
-        }
-        i++;
-    }
+    	while (format[i])
+    	{
+        	if (format[i] == '%')
+        	{
+            		j = handle_format_specifier(format, i + 1, &args, buffer);
+            		if (j == -1)
+                		return (-1);
+            		len += j;
+            		i++;
+        	}
+        	else
+        	{
+            		buffer[len++] = format[i];
+        	}
+        	i++;
+    	}
 
-    va_end(args);
+    	va_end(args);
 
-    buffer[len] = '\0';
+    	buffer[len] = '\0';
 
-    /* TODO: Print the buffer to the console. */
-
-    return (len);
+   	return (len);
 }
 /**
  * handle_format_specifier - Handles a format specifier.

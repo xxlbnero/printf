299941872a9dcb3c2b2674f9c153fee52660b60e
Nerus <n.nwoko2023@gmail.com>
2023-03-27 10:12:59 -0700

    main header file containing function prototypes

--- main.h
+++ main.h
@@ -1,7 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>	/* printf, etc*/
+#include <limits.h>	/* INT_MAX, etc */
+#include <stdlib.h>	/* malloc() */
+#include <unistd.h>	/* write() */
+#include <stdarg.h>	/* variadic function */
+
+
 int _putchar(int c);
+int _printf(const char *format, ...);
+
 
 
 #endif	/* MAIN_H */
